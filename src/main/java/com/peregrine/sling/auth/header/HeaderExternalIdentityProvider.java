5e4fc37a7871a014a58b54fac987e8b9626ca558
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-03-15 11:31:13 -0700

    Reached PoC stage. We can successfully log in with a header. Now the real work begins. Starting clean-up.

---
+++ src/main/java/com/peregrine/sling/auth/header/HeaderExternalIdentityProvider.java
@@ -0,0 +1,165 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.peregrine.sling.auth.header;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.apache.jackrabbit.oak.spi.security.ConfigurationParameters;
+import org.apache.jackrabbit.oak.spi.security.authentication.external.*;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Credentials;
+import javax.security.auth.login.LoginException;
+import java.util.*;
+
+@Component(
+        name = "Header External Identity Provider",
+        service = ExternalIdentityProvider.class,
+        immediate = true
+)
+/**
+ * The implementation is based on @see org.apache.jackrabbit.oak.exercise.security.authentication.external.CustomExternalIdentityProvider.class
+ *
+ * @author <a href="mailto:gg@headwire.com">Gaston Gonzalez</a>
+ */
+public class HeaderExternalIdentityProvider implements ExternalIdentityProvider
+{
+    private final Logger logger = LoggerFactory.getLogger(HeaderExternalIdentityProvider.class);
+
+    public static final String NAME = "HeaderExternalIdentityProvider";
+
+    private Map<String, Set<String>> userGroupMap = new HashMap<String, Set<String>>();
+    private Set<String> groupIds = new HashSet<String>();
+
+    public HeaderExternalIdentityProvider()
+    {
+        // Default constructor
+        logger.info("Default constructor called");
+    }
+
+    @Activate
+    public void activate(Map<String, Object> properties)
+    {
+        ConfigurationParameters config = ConfigurationParameters.of(properties);
+        logger.info("Activated IDP: '{}' with config: '{}'", getName(), config);
+    }
+
+    @Modified
+    public void modified(Map<String, Object> properties)
+    {
+        ConfigurationParameters config = ConfigurationParameters.of(properties);
+        logger.info("Modified IDP: '{}' with config: '{}'", getName(), config);
+    }
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public ExternalIdentity getIdentity(ExternalIdentityRef externalIdentityRef) throws ExternalIdentityException
+    {
+        if (getName().equals(externalIdentityRef.getProviderName()))
+        {
+            String id = externalIdentityRef.getId();
+            return getUser(id);
+        } else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public ExternalUser getUser(final String userId) throws ExternalIdentityException
+    {
+        return new ExternalUser()
+        {
+
+            @Override
+            public ExternalIdentityRef getExternalId()
+            {
+                return new ExternalIdentityRef(userId, getName());
+            }
+
+            @Override
+            public String getId()
+            {
+                return userId;
+            }
+
+            @Override
+            public String getPrincipalName()
+            {
+                return "p_" + getExternalId().getString();
+            }
+
+            @Override
+            public String getIntermediatePath()
+            {
+                return null;
+            }
+
+            @Override
+            public Iterable<ExternalIdentityRef> getDeclaredGroups() throws ExternalIdentityException
+            {
+                return ImmutableSet.of();
+            }
+
+            @Override
+            public Map<String, ?> getProperties()
+            {
+                return ImmutableMap.of();
+            }
+        };
+    }
+
+    @Override
+    public ExternalUser authenticate(Credentials credentials) throws ExternalIdentityException, LoginException
+    {
+        if (credentials instanceof HeaderCredentials)
+        {
+            String userId = ((HeaderCredentials) credentials).getUserId();
+            return getUser(userId);
+        } else
+        {
+            throw new LoginException("Unsupported credentials");
+        }
+    }
+
+    @Override
+    public ExternalGroup getGroup(String s) throws ExternalIdentityException
+    {
+        return null;
+    }
+
+    @Override
+    public Iterator<ExternalUser> listUsers() throws ExternalIdentityException
+    {
+        throw new UnsupportedOperationException("listUsers");
+    }
+
+    @Override
+    public Iterator<ExternalGroup> listGroups() throws ExternalIdentityException
+    {
+        throw new UnsupportedOperationException("listGroups");
+    }
+}
--- src/main/java/com/peregrine/sling/auth/header/HeaderExternalLoginModule.java
+++ src/main/java/com/peregrine/sling/auth/header/HeaderExternalLoginModule.java
@@ -1,9 +1,17 @@
 package com.peregrine.sling.auth.header;
 
-import org.apache.jackrabbit.oak.spi.security.ConfigurationParameters;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.api.Root;
+import org.apache.jackrabbit.oak.namepath.NamePathMapper;
+import org.apache.jackrabbit.oak.plugins.value.ValueFactoryImpl;
 import org.apache.jackrabbit.oak.spi.security.authentication.AbstractLoginModule;
 import org.apache.jackrabbit.oak.spi.security.authentication.PreAuthenticatedLogin;
+import org.apache.jackrabbit.oak.spi.security.authentication.external.*;
+import org.apache.jackrabbit.oak.spi.whiteboard.Whiteboard;
+import org.apache.jackrabbit.oak.spi.whiteboard.WhiteboardUtils;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.metatype.annotations.Designate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,16 +36,23 @@ import java.util.Set;
 public class HeaderExternalLoginModule extends AbstractLoginModule
 {
     private final Logger logger = LoggerFactory.getLogger(HeaderExternalLoginModule.class);
+    private static final int MAX_SYNC_ATTEMPTS = 3;
 
     static Set<Class> SUPPORTED_CREDENTIALS = new HashSet<Class>();
-    static {
+
+    static
+    {
         SUPPORTED_CREDENTIALS.add(HeaderCredentials.class);
     }
 
-    private ConfigurationParameters osgiConfig;
+    private ExternalIdentityProvider externalIdentityProvider;
 
+    private SyncHandler syncHandler;
 
-    public HeaderExternalLoginModule() {
+    private ExternalUser externalUser;
+
+    public HeaderExternalLoginModule()
+    {
         logger.debug("Initializing default constructor");
     }
 
@@ -52,20 +67,94 @@ public class HeaderExternalLoginModule extends AbstractLoginModule
     {
         Credentials credentials = getCredentials();
 
-        if (credentials instanceof HeaderCredentials) {
+        if (credentials instanceof HeaderCredentials)
+        {
             final String userId = ((HeaderCredentials) credentials).getUserId();
-            if (userId == null) {
+            if (userId == null)
+            {
                 logger.warn("Could not extract userId from credentials");
-            } else {
+            } else
+            {
                 sharedState.put(SHARED_KEY_PRE_AUTH_LOGIN, new PreAuthenticatedLogin(userId));
                 sharedState.put(SHARED_KEY_CREDENTIALS, new SimpleCredentials(userId, new char[0]));
                 sharedState.put(SHARED_KEY_LOGIN_NAME, userId);
-                logger.info("Login succeeded with trusted user: {}", userId);
+                logger.info("Adding pre-authenticated login user '{}' to shared state.", userId);
+
+                handleUserSync(userId);
             }
         }
 
         // subsequent login modules need to succeed and process the 'PreAuthenticatedLogin'
         return false;
+    }
+
+    private void handleUserSync(final String userId)
+    {
+        try
+        {
+            SyncedIdentity syncedIdentity = null;
+            UserManager userMgr = getUserManager();
+            externalUser = externalIdentityProvider.getUser(userId);
+
+            if (userId != null && userMgr != null)
+            {
+                syncedIdentity = syncHandler.findIdentity(userMgr, userId);
+                if (syncedIdentity != null)
+                {
+                    logger.info("Found identity: '{}' for user: '{}'", syncedIdentity, userId);
+                    // TODO: Should we sync existing users?
+                    return;
+                }
+
+                logger.info("Could not find existing identity: '{}'", userId);
+
+                Root root = getRoot();
+                if (null == root)
+                {
+                    throw new SyncException("Cannot synchronize user. root == null");
+                }
+
+                UserManager userManager = getUserManager();
+                if (userManager == null)
+                {
+                    throw new SyncException("Cannot synchronize user. userManager == null");
+                }
+                logger.debug("Got UserManager");
+
+
+                userManager.createUser(userId, (new char[0]).toString());
+                logger.info("Created user: '{}'", userId);
+
+                int numAttempt = 0;
+                while (numAttempt++ < MAX_SYNC_ATTEMPTS)
+                {
+                    SyncContext context = null;
+                    try
+                    {
+                        context = syncHandler.createContext(externalIdentityProvider, userManager,
+                                new ValueFactoryImpl(root, NamePathMapper.DEFAULT));
+                        context.sync(userId);
+                        root.commit();
+                        logger.info("Synced user: '{}'", externalUser.getId());
+                        return;
+                    } catch (CommitFailedException e)
+                    {
+                        root.refresh();
+                    } finally
+                    {
+                        if (context != null)
+                        {
+                            context.close();
+                        }
+                    }
+                }
+                throw new SyncException("User synchronization failed during commit after " + MAX_SYNC_ATTEMPTS + " attempts");
+
+            }
+        } catch (Exception e)
+        {
+            logger.error("Error syncing user: '{}'", userId, e);
+        }
     }
 
     @Override
@@ -79,9 +168,57 @@ public class HeaderExternalLoginModule extends AbstractLoginModule
     @Override
     public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options)
     {
-        // TODO:  Do we need to override this method?
         logger.debug("initialize() called");
         super.initialize(subject, callbackHandler, sharedState, options);
+
+        // 1. Get Whiteboard
+        Whiteboard whiteboard = getWhiteboard();
+        if (null == whiteboard)
+        {
+            logger.error("Header login module needs Whiteboard.");
+            return;
+        }
+        logger.debug("Got Whiteboard");
+
+        // 2. Get Identity Provider Manager
+        ExternalIdentityProviderManager externalIdentityProviderManager = WhiteboardUtils.getService(whiteboard, ExternalIdentityProviderManager.class);
+        if (null == externalIdentityProviderManager)
+        {
+            logger.error("Header login module needs ExternalIdentityProviderManager.");
+            return;
+        }
+        logger.debug("Got ExternalIdentityProviderManager");
+
+        // 3. get Identity Provider
+        externalIdentityProvider = externalIdentityProviderManager.getProvider(HeaderExternalIdentityProvider.NAME);
+        if (null == externalIdentityProvider)
+        {
+            logger.error("Header login module needs ExternalIdentityProvider. Can't get IDP with name: '{}'",
+                    HeaderExternalIdentityProvider.NAME);
+            return;
+        }
+        logger.debug("Got ExternalIdentityProvider");
+
+
+        // 4. get Sync Manager
+        SyncManager syncMgr = WhiteboardUtils.getService(whiteboard, SyncManager.class);
+        if (null == syncMgr)
+        {
+            logger.error("Header login module needs SyncManager.");
+            return;
+        }
+        logger.debug("Got SyncManager");
+
+        // 5. Get Syn Handler
+        // TODO: Remember to create the OSGi configuration
+        String syncHandlerName = "default";
+        syncHandler = syncMgr.getSyncHandler(syncHandlerName);
+        if (null == syncHandler)
+        {
+            logger.error("Header login module needs SyncHandler. Can't get SyncHandler: '{}'", syncHandlerName);
+            return;
+        }
+        logger.debug("Got SyncHandler: '{}'", syncHandler);
     }
 
     @Override
